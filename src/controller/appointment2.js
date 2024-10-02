a75f6f13ade1b80a758cd768084f1ffb9fd396f4
NithikonIndy <nithikon4410@hotmail.com>
2024-10-02 15:41:53 +0700

    appoint2

--- src/controller/appointment2.js
+++ src/controller/appointment2.js
@@ -87,7 +87,7 @@ export const checkappointment2 = async (request) => {
 SELECT u.firstname_lastname, u.studentid, ucr2.start_datetime, ucr2.end_datetime, ucr2.room, ucr2.event_id
 FROM users u
 INNER JOIN user_conseling_room1 ucr2 ON u.personid = ucr2.personid
-WHERE u.studentid = $1
+WHERE u.studentid = $1 and ucr.start_datetime::timestamptz > NOW()
 limit 1 ;
         `;
 
